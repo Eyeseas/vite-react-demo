76c3e078851e22cd127d9a19d924a77e9e7addef
kong2022 <2710404562@qq.com>
2022-03-30 17:50:39 +0800

    init some framework

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import  Book from "./pages/Book";
+
+function App() {
+
+  return (
+    <div className="App">
+      <Book></Book>
+    </div>
+  )
+}
+
+export default App
